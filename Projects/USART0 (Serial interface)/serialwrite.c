4cf661e956d897ae3f1f1a8bad906d065b431a46
Tech Rick <45835951+richu101@users.noreply.github.com>
2020-09-01 19:59:17 +0530

    change by geocheatten

--- Projects/USART0 (Serial interface)/serialwrite.c
+++ Projects/USART0 (Serial interface)/serialwrite.c
@@ -8,81 +8,77 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define F_CPU 16000000
 #include <util/delay.h>
 
-#define BUAD 9600
-#define UBBR_VAL ((F_CPU/16/BUAD)-1)
+#define F_CPU 16000000
+#define BAUD 9600
+#define UBBR_VAL ((F_CPU/16/BAUD)-1)
+#define buffersize 128
 
-#define buffersize  128
-char serialbuffer [buffersize];
-uint8_t readpos = 0;
-uint8_t writepos = 0;
+char serialbuffer[buffersize];
+uint8_t readpos=0;
+uint8_t writepos=0;
+uint8_t Read_val = 0;
 
 void appendserial(char c);
-void writeserial(char c[]); 
+void writeserial(char c[]);
+void serialbegin();
 
-uint8_t Read_val = 0;
- void serialbegin();
- 
 int main(void)
 {
- serialbegin();
-
- sei();
- 
- writeserial("Hello");
- writeserial("WorLdD");
-    while (1) 
-    {
-		UDRE0 = 3;
-    }
-	return 0;
+  serialbegin();
+  sei();
+  writeserial("Hello");
+  writeserial("World");
+  while(1)
+  {
+    //UDRE0 = 0;
+  }
+  return 0;
 }
 
- void serialbegin()
- {
-	 UCSR0B = (1<<TXEN0) | (1<<TXCIE0) ;
-	 UCSR0C = (1<<UCSZ00)| (1<<UCSZ01) ;
-	 UBRR0L = UBBR_VAL;
-	 UBRR0H = UBBR_VAL >> 8;
- }
- /*
- void writeserial(char c[])
- {
- }
- */
+void serialbegin()
+{
+  UCSR0B = (1<<TXEN0) | (1<<TXCIE0);
+  UCSR0C = (1<<UCSZ00) | (1<<UCSZ01);
+  UBRR0L = UBBR_VAL;
+  UBRR0H = UBBR_VAL >> 8;
+}
+
 void writeserial(char c[])
 {
-	for (uint8_t i=0;i<10;i++)
-	{
-		appendserial(c[i]);
-	}
-	if (UCSR0A & (1<<UDRE0))
-	{
-		UDR0 = 0;
-	}
+  //for(uint8_t i=0;i<10;i++)
+  while(*c != 0)
+  {
+    appendserial(*c);
+    c++;
+  }
+  appendserial(' ');
+  if(UCSR0A & (1<<UDRE0))
+  {
+    UDR0 = 0;
+  }
 }
 
 void appendserial(char c)
 {
-	serialbuffer[writepos] = c;
-	writepos++;
-	if (writepos >= buffersize)
-	{
-		writepos =0;
-	}
+  serialbuffer[writepos] = c;
+  writepos++;
+  if(writepos >= buffersize)
+  {
+    writepos = 0;
+  }
 }
-ISR(USART_TX_vect)
+
+ISR(USART_TX_vect)  
 {
-	if (readpos != writepos)
-	{
-		UDR0 = serialbuffer[readpos];
-		readpos++;
-		if (readpos <= buffersize)
-		{
-			readpos++;
-		}
-	}
-	
+  if(readpos != writepos)
+  {
+    UDR0 = serialbuffer[readpos];
+    readpos++;
+   // if(readpos <= buffersize)
+   // {
+    //  readpos++;
+   // }
+  }
 }
