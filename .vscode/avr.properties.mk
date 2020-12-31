4d3fc1ad58f75bc3e5edded01801c03a4059dc38
richu101 <richubini101@gmail.com>
2020-12-31 08:22:46 +0000

    changes

---
+++ .vscode/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = /usr/bin/avr-c++
+AVR.source.libraries = 
+AVR.device.type = 
+AVR.device.frequency = 
---
+++ intrrepts/pin_change_intrrept.c
@@ -0,0 +1,13 @@
+#include<avr/io.h>
+#include<util/delay.h>
+
+int min(void)
+|{
+    DDRB |= (1<<2)
+    while(1)
+    {
+    PORTB = (1<<2);
+    _delay_ms(1000);
+    }
+return (0);
+}
