b3d9e8716374a6cbd100cd62f433932d8c97dff8
richu101 <richubini101@gmail.com>
2021-01-12 03:48:56 +0000

    test code added

---
+++ external_intrrept.c
@@ -0,0 +1,90 @@
+#ifndef __AVR_ATmega328P__ 
+    #define __AVR_ATmega328P__
+#endif
+#define F_CPU 16000000UL
+
+#include<avr/io.h>
+#include<util/delay.h>
+#include<avr/interrupt.h>
+#include"liberaries/USART_liberry/usart.h"
+
+unsigned char str[75] = "good morning  angela mummy   ";
+unsigned char str2[50] = "mummy i can arrange a light show for you mummy";
+
+
+int main(void)
+{
+    DDRB = 0xff; 
+    while(1)
+    {
+        _delay_ms(3000);
+        serialbegin();
+        for(int i = 0;i<=75;i++)
+        {
+        serialwritechar(str[i]);
+        serialwritechar(10);
+        
+        }
+        _delay_ms(2000);
+        
+
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(400);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(200);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(400);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(200);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+        _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(400);
+        (PORTB ^= 0xff);
+       _delay_ms(100);
+        (PORTB ^= 0xff);
+       _delay_ms(200);
+        (PORTB ^= 0xff);
+       _delay_ms(1000);
+        (PORTB ^= 0xff);
+
+
+    }
+    return (0);
+
+}
