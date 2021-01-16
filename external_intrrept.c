f588d1e8050e21aa2c4c42a904aa1fcfe3cd5f26
richu101 <richubini101@gmail.com>
2021-01-16 04:45:54 +0000

    external intrrept code added

--- external_intrrept.c
+++ external_intrrept.c
@@ -1,3 +1,14 @@
+/*
+
+#  This part is to define the board that we are using 
+#  You can see all  the board list that are availabele in the ave/io.h lib by 
+#  peacking to the header just select the avr/io.h in the #include part and press F12
+#  press ctrl + f and search for the board that u are looking for
+#  If your board is available then u can see the board name unde __AVR_your-board__
+#  Here am using Atmega328p SO   __AVR_ATmega328P__ 
+
+*/  
+
 #ifndef __AVR_ATmega328P__ 
     #define __AVR_ATmega328P__
 #endif
@@ -6,86 +17,40 @@
 #include<avr/io.h>
 #include<util/delay.h>
 #include<avr/interrupt.h>
-#include"liberaries/USART_liberry/usart.h"
 
-unsigned char str[75] = "good morning  angela mummy   ";
-unsigned char str2[50] = "mummy i can arrange a light show for you mummy";
+ISR(INT0_vect)
+{
+    // falling edge will trigger this intrrept
+    // if(bit_is_clear(PIND,PIND2))
+    //{
+        PORTB = 0;
+        _delay_ms(1000);
+    //}
 
+}
+void external_intrrept_init()
+{
+    EIMSK |= (1<<INT0);
+    EICRA |= (1<<ISC01);  //The falling edge of INT0 generates an interrupt request.
+    sei();
+}
 
 int main(void)
 {
-    DDRB = 0xff; 
+    
+    DDRB = 0xff;
+    DDRD |= (0<<PD2);  //setting PD2 pin as output mode
+    PORTD |= (1<<PD2); //enabling the internal pull up
+    external_intrrept_init();
+    
     while(1)
     {
-        _delay_ms(3000);
-        serialbegin();
-        for(int i = 0;i<=75;i++)
-        {
-        serialwritechar(str[i]);
-        // serialwritechar(10);
-
-        }
-        serialwritechar(10); // ASCII 10 = /n = new line
-        _delay_ms(2000);
-        
-
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(400);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(200);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(400);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(200);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-        _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(400);
-        (PORTB ^= 0xff);
-       _delay_ms(100);
-        (PORTB ^= 0xff);
-       _delay_ms(200);
-        (PORTB ^= 0xff);
-       _delay_ms(1000);
-        (PORTB ^= 0xff);
-
+    PORTB = 0xff;
+    _delay_ms(1000);
+    // PORTB = (0<<5);
+    // _delay_ms(100);
 
     }
-    return (0);
+return (0);
 
 }
