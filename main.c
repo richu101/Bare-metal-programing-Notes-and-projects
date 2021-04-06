c03c5fbd3e25458c1386205c5d3c38ade379a4ac
richu101 <richubini101@gmail.com>
2021-04-07 01:40:10 +0900

    ok

--- main.c
+++ main.c
@@ -1,48 +1,56 @@
-/*
-
-#  This part is to define the board that we are using 
-#  You can see all  the board list that are availabele in the ave/io.h lib by 
-#  peacking to the header just select the avr/io.h in the #include part and press F12
-#  press ctrl + f and search for the board that u are looking for
-#  If your board is available then u can see the board name unde __AVR_your-board__
-#  Here am using Atmega328p SO   __AVR_ATmega328P__ 
-
-*/  
-
 #ifndef __AVR_ATmega328P__ 
     #define __AVR_ATmega328P__
 #endif
 #define F_CPU 16000000UL
-#define threshold 12000
+
 #include<avr/io.h>
 #include<util/delay.h>
 #include<avr/interrupt.h>
-#include<avr/power.h>
-#include"USART_liberry/usart.c"
-int status = 0;
-int main(void)
-{   
-    DDRB = 0xff;
-    DDRD = (0<<2);
+#include"liberaries/USART_liberry/usart.c"
+
+
+volatile uint8_t i;
+
+ISR(INT0_vect)
+{
+    // falling edge will trigger this intrrept
+    if(bit_is_set(PIND,PIND2))
+    {
+        PORTB ^= (0xff);
+        if (PORTB)
+        {
+            transmitByte(12);
+            printString("on");
+        }
+        if (PORTB==0)
+        {
+            transmitByte(12);
+            printString("off");
+        }
+        
+        
+        
+    }
+
+
+}
+void external_intrrept_init()
+{
+    EIMSK |= (1<<INT0);
+    EICRA |= (1<<ISC01);
+    EICRA |= (1<<ISC00);  //Any logic change in int0 will trigger the intrrept 
+    sei();
+}
+int main(void)          
+{
+    DDRB = 0xff; 
     initUSART();
-    while (1)
+    DDRD |= (0<<PD2);  //setting PD2 pin as output mode
+    external_intrrept_init();
+    while(1)
     {
-    if(bit_is_clear(PIND,PIND2))
-    {
-        status = !status;
-        PORTB = (status<<PB5);
-    }
     
-    if (status == 1)
-    {
-        printString("off");
-    }
-    else
-    {
-        printString("on");
-    }
-    while (bit_is_clear(PIND,PIND2));
-    _delay_ms(500);
     }
     return (0);
+
 }
