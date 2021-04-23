a8141af5c7456b7f4f914c7db2d93f04e291a711
richu101 <richubini101@gmail.com>
2021-04-24 02:03:15 +0900

    init uart" ""

--- main.c
+++ main.c
@@ -7,44 +7,14 @@
 #include<util/delay.h>
 #include<avr/interrupt.h>
 #include"liberaries/USART_liberry/usart.c"
-
-
-volatile uint8_t i;
-
-ISR(INT0_vect)
+void init_timer1()
 {
-    // falling edge will trigger this intrrept
-    if(bit_is_set(PIND,PIND2))
-    {
-        PORTB ^= (0xff);
-        if (PORTB)
-        {
-            transmitByte(12);
-            printString("on");
-        }
-        if (PORTB==0)
-        {
-            transmitByte(12);
-            printString("off");
-        }
-        
-    }
-
-
+    
 }
-void external_intrrept_init()
-{
-    EIMSK |= (1<<INT0);
-    EICRA |= (1<<ISC01);
-    EICRA |= (1<<ISC00);  //Any logic change in int0 will trigger the intrrept 
-    sei();
-}
+
 int main(void)          
 {
-    DDRB = 0xff; 
     initUSART();
-    DDRD |= (0<<PD2);  //setting PD2 pin as output mode
-    external_intrrept_init();
     while(1)
     {
     }
