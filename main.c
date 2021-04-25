ff8508598a88cec45e9223370ed5984b5f0ce3d3
richu101 <richubini101@gmail.com>
2021-04-25 11:30:09 +0900

    timer test code

--- main.c
+++ main.c
@@ -6,16 +6,18 @@
 #include<avr/io.h>
 #include<util/delay.h>
 #include<avr/interrupt.h>
-#include"liberaries/USART_liberry/usart.c"
+#include"liberaries/USART_liberry/usart.h"
 void init_timer1()
 {
     TCCR1B |= (1<<CS11) | (1<<CS10);
-    // set the clock speed 1MHz / 64
+    // set the clock speed 16MHz / 64
+    // 16/64 tick of the clock will increase the timer value by 1
 }
 
 int main(void)          
 {
     initUSART();
+    init_timer1();
     while(1)
     {
     }
