106eab3888a4e4cdbf4579cde99f7d1c7121d709
richu101 <richubini101@gmail.com>
2021-05-01 02:19:03 +0900

    timer example code

--- timers/timmer1.c
+++ timers/timmer1.c
@@ -17,7 +17,7 @@
 void init_timer1()
 {
     TCCR1B |= (1<<CS11) | (1<<CS10);
-    // set the clock speed 16MHz / 64
+    // set the (clk i/o) clock speed 16MHz / 64
     // 16/64 tick of the clock will increase the timer value by 1
 }
 
