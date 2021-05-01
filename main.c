16d1ac8fc3a4254d3c0f9aac87ae814a3b7d83b0
richu101 <richubini101@gmail.com>
2021-05-01 12:31:30 +0900

    organ usng timer1

--- main.c
+++ main.c
@@ -8,7 +8,9 @@
 #include<avr/interrupt.h>
 #include"liberaries/USART_liberry/usart.c"
 void init_timer1()
-{
+{   
+    TCCR0A |= (1<<WGM01);
+    
     TCCR1B |= (1<<CS11) | (1<<CS10);
     // set the clock speed 16MHz / 64
     // 16/64 tick of the clock will increase the timer value by 1
@@ -16,34 +18,12 @@ void init_timer1()
 
 int main(void)          
 {   
-    char byte;
-    uint16_t timerval;
-    initUSART();
-    init_timer1();
-    DDRB = (1<<PB5);
+   
     while(1)
     
     {
 
-        printString("ready .... \n");
-        byte = receiveByte();
-        PORTB |= (1<<PB5);
-        _delay_ms(500);
-        PORTB = 0;
-        printString("Go >>");
-        TCNT1 = 0;
-        if(bit_is_set(PIND,PD2))
-        {
-            printString("no cheeting..\n    ");
-        }
-        else
-        {   
-            loop_until_bit_is_set(PIND,PD2);
-            timerval = TCNT1;
-            TCNT1 = 0;
-            printString("your reaction time is .....");
-            printInt(timerval);
-            transmitByte(13); // clear the terminal set the curser to 0 th position
+        
         }
         
         
