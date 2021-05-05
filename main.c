2cc2747842db96ffc74dc029baad26bfce7cf45a
richu101 <richubini101@gmail.com>
2021-05-06 02:57:23 +0900

    timer discriptation added

--- main.c
+++ main.c
@@ -7,37 +7,44 @@
 #include<util/delay.h>
 #include<avr/interrupt.h>
 #include"liberaries/USART_liberry/usart.c"
-void init_timer1()
+static inline void init_timer1()
 {   
-    TCCR0A |= (1<<WGM01);
+    // TCCR1A |= (1<<WGM12);
     // enable the counter to work in ctc(clear timer on compare) mode
-    TCCR0A |= (1<<COM0A0);
+    TCCR1A |= (1<<COM1A0);
     // toggle OC0A on compare match
-    TCCR1B |= (1<<CS11) | (1<<CS10);
+    TCCR1B |= (1<<CS10) | (1<<WGM10) | (1<<CS11);
     // set the clock speed 16MHz / 64
     // 16/64 tick of the clock will increase the timer value by 1
     
 }
-void playNote(uint16_t wavelength, uint16_t duration) {
+static inline void playNote(uint8_t wavelength, uint8_t duration) {
 
-    DDRD |= (1<<PD6);
-    OCR0A = wavelength;
+    OCR1A = wavelength;
+    DDRB |= (1<<PB1);
     while (duration)
     {
-        _delay_ms(1);
+        _delay_us(1);
         duration--;
     }
-    DDRD &= ~(1<<PD6);
+    DDRB &= ~(1<<PB1);
   
 }
 int main(void)          
 {   
-    DDRB |= (1<<PB5);
+    // DDRB |= (1<<PB5);
     init_timer1();
     while(1)
     
     {   
-        playNote(25,3000);
+//        DDRB |= (1<<PB1);
+//        PORTB ^= (1<<PB1);
+//        _delay_ms(100);
+        playNote(25,20);
+        playNote(30,200);
+        playNote(50,100);
+        playNote(48,150);
+        _delay_ms(4000);
     }
         
         
