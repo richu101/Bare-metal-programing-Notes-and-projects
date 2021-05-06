ee4a4990a2765bf3c961a1d5aa330423d2573a37
richu101 <richubini101@gmail.com>
2021-05-06 17:10:17 +0900

    timer discriptation added

--- main.c
+++ main.c
@@ -11,9 +11,9 @@ static inline void init_timer1()
 {   
     // TCCR1A |= (1<<WGM12);
     // enable the counter to work in ctc(clear timer on compare) mode
-    TCCR1A |= (1<<COM1A0);
+    TCCR1A |= (1<<COM1A0)  ;
     // toggle OC0A on compare match
-    TCCR1B |= (1<<CS10) | (1<<WGM10) | (1<<CS11);
+    TCCR1B |= (1<<CS10)| (1<<CS11) | (1<<WGM12);
     // set the clock speed 16MHz / 64
     // 16/64 tick of the clock will increase the timer value by 1
     
@@ -27,7 +27,7 @@ static inline void playNote(uint8_t wavelength, uint8_t duration) {
         _delay_ms(1);
         duration--;
     }
-    DDRB &= ~(1<<PB1);
+    DDRB &= (0<<PB1);
 }
 int main(void)          
 {   
@@ -39,11 +39,22 @@ int main(void)
 //        DDRB |= (1<<PB1);
 //        PORTB ^= (1<<PB1);
 //        _delay_ms(100);
-        playNote(25,20);
-        playNote(30,200);
-        playNote(50,100);
-        playNote(48,150);
-        _delay_ms(4000);
+        playNote(43,20);
+        playNote(13,200);
+        playNote(13,100);
+        playNote(31,150);
+        _delay_ms(400);
+        playNote(50,200);
+        playNote(100,200);
+        playNote(60,100);
+        playNote(23,150);
+        _delay_ms(400);
+        playNote(200,140);
+        playNote(100,150);
+        playNote(130,140);
+        playNote(253,105);
+        _delay_ms(400);
+
     }
         
         
