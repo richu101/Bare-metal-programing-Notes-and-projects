d656c345a41415a86dc9cca7dafb5935373d0ecf
richu101 <richubini101@gmail.com>
2021-09-01 13:53:52 +0900

    hardware pwm test on process

--- main.c
+++ main.c
@@ -18,15 +18,24 @@
 
 void timer1init(uint8_t dutycycle)
 {
+// Timer 1 (16 bit timer) initilazation
+
     TCCR1B |= (1<<WGM12) ; // | fast pwm with 8 bit
     TCCR1A |= (1<<WGM10) ; // | fast pwm with 8 bit
     TCCR1B |= (1<<CS11);   // | clk prescalr div / 8
-    TCCR1A |= (1<<COM1A1); // | Clear OC1B on compare match when up-counting
+    TCCR1A |= (1<<COM1A1); // | Clear OC1A on compare match when up-counting
     TCCR1A |= (1<<COM1B1); // | Clear OC1B on compare match when up-counting
+// Timer 2 (8 bit timer) initilazation
+
+    TCCR2A |= (1<<WGM20) ; // | fast pwm with 8 bit
+    TCCR2A |= (1<<WGM21) ; // | fast pwm with 8 bit
+    TCCR2B |= (1<<CS21);   // | clk prescalr div / 8
+    TCCR2A |= (1<<COM2A1); // | Clear OC2A on compare match, set OC2A at BOTTOM,(non-inverting mode).
+    
 }
 
 int main()
 {
-
+    DDRB
     return (0);
 }
