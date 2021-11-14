becbdde8c536acd43135338fbb29a925fdbd54c5
richu101 <richubini101@gmail.com>
2021-11-14 12:46:44 +0530

    i2c mt mode test code

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 |   
 |   This code works fine with mcu workin in 16Mhz clk freequency
 |   
-|
+|   
 |   
 |   
 |
@@ -18,14 +18,48 @@
 
 #include<avr/io.h>
 #include<util/delay.h>
+void i2c_init()
+{
+	TWBR = 0x62;		//--- Baud rate is set by calculating the of TWBR see Details for Reference
+	TWCR = (1<<TWEN);	//--- Enable I2C
+	TWSR = 0x00;		//--- Prescaler set to 1
+}
+void i2c_start_transmit(){
+        TWCR = 0x4A;
+        while(!(TWCR & (1<<TWINT))); 
+}
+
+void i2c_stop_transmit(){
+        TWCR = 0x49;
+        while(!(TWCR & (1<<TWINT))); 
+}
+
+void i2ctransmit_data(unsigned int a){
+        TWCR = 0x48;
+        TWDR = a;
+
+        while(!(TWCR & (1<<TWINT)));
+}
+
+void i2caddr (uint8_t a, uint8_t b  )
+{
+        // The value of b = 1 for (READ) b = 0 (WRITE)
+        if (b == 0) a |=(0<<1);
+        if (b == 1) a |=(1<<1);
+        i2ctransmit_data(a);              
+}
 
 int main()
 {
         DDRB = (1<<PB5); // Set the DDRB port in OUTPUT mode
         while(1)
-        {
+        {       
+                i2c_start_transmit();
+                i2caddr(8,0);
+                
                 PORTB ^= (1<<PB5); // Toggle the PB5 port in every sec
                 _delay_ms(1000);
+                
         }
 return (0);
 }
